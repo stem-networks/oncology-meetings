74e961b605acb506bfb30beb2f74731c33815222
stem-networks <stem.networks.india@gmail.com>
2025-10-16 11:29:27 +0530

    Venue added -Srilakshmi

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -119,7 +119,7 @@ const Header: React.FC<HeaderProps> = ({ generalData, registerData }) => {
                   </Link>
                   {presenterFee && <span> For Only ${presenterFee}</span>}
                 </li>
-                <li className="text-right">{general.venue_p1 || ""}</li>
+                {/* <li className="text-right">{general.venue_p1 || ""}</li> */}
               </ul>
             </div>
           </div>
@@ -127,7 +127,7 @@ const Header: React.FC<HeaderProps> = ({ generalData, registerData }) => {
 
         {/* Header Upper */}
         <div className="header-upper">
-          <div className="auto-container">
+          <div className="container-fluid">
             <div style={{ justifyContent: "space-between", display: "flex" }}>
               <div className="logo-main-block">
                 <Link
@@ -146,7 +146,7 @@ const Header: React.FC<HeaderProps> = ({ generalData, registerData }) => {
                 </Link>
               </div>
 
-              {/* <div className="venue-header-block">
+              <div className="venue-header-block">
                 {general.venue_p1 ? (
                   <div className="map_wrap156" style={{ textAlign: 'start' }}>
                     <Image
@@ -176,7 +176,7 @@ const Header: React.FC<HeaderProps> = ({ generalData, registerData }) => {
                 ) : null}
               </div>
 
-              <div className='approved-provider-block'>
+              {/*  <div className='approved-provider-block'>
                 <Image src="/images/images/cpd.jpg" alt="CPD Accredited Conference" className='approved-prov-img' title="CPD Accredited Conference" width={360} height={130} />
               </div> */}
 
